2b0406541a900ec5b7011847051b89970beeeacf
YousefV10 <yusifrv-bpa202@code.edu.az>
2025-10-09 12:39:46 +0400

    Add project files.

---
+++ Array,loops-2/Program.cs
@@ -0,0 +1,98 @@
+﻿
+#region Task1 
+//int[] numbers1 = [1, 2, 3];
+//bool Istrue = true;
+//int[] numbers2 = [1, 5, 2, 4, 2];
+//if (numbers1.Length > numbers2.Length) {
+
+//    Console.WriteLine("1ci array 2 ciden qisa olmalidir ");
+
+
+
+//}
+//else {
+//    for (int i = 0; i < numbers1.Length; i++)
+//    {
+//        bool found = false;
+//        for (int j = 0; j < numbers2.Length; j++)
+//        {
+//            if (numbers1[i] == numbers2[j])
+//            {
+//                found = true;
+//                break;
+//            }
+//        }
+
+
+//        if (found != false)
+//        {
+//            Istrue = false;
+//            break;
+//        }
+//    }
+
+//    if (Istrue != false)
+//    {
+//        Console.WriteLine("hamisi movcud deyil ");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Hamisi movcuddur");
+//    }
+//}
+
+
+#endregion
+
+#region Task2
+
+//int[] arrayRes = [1, 1, 2, 3, 4, 5, 5, 6, 8, 11, 11];
+//int number1 = arrayRes[0];
+//int number2 = arrayRes[1];
+//for (int i = 1; i < arrayRes.Length; i++) {
+//    if (arrayRes[i] == arrayRes[i-1])
+//    {
+//        Console.WriteLine(arrayRes[i]);
+//    }
+
+//}
+#endregion
+
+#region Task3 - Yaza bilmədim 
+#endregion
+
+#region Task4
+//int[] a = { 5,3,7,9,2,8,3756,347};
+//int[] sadeEdedler = new int[10];
+
+//for(int i =0; i<a.Length; i++)
+//{
+//    int b = 0;
+//    for (int j=1; j <= a[i]; j++)
+//    {
+
+//        if (a[i] % j == 0) {
+//            b++;
+
+//        }
+
+
+//    }
+//    if (b == 2)
+//    {
+//        sadeEdedler[i] = a[i];
+//    }
+//}
+
+//Array.Sort(sadeEdedler);
+//Array.Reverse(sadeEdedler);
+//while (sadeEdedler[sadeEdedler.Length - 1] == 0)
+//{
+//    Array.Resize(ref sadeEdedler, sadeEdedler.Length - 1);
+//}
+
+//foreach (var item in sadeEdedler)
+//{
+//    Console.WriteLine(item);
+//}
+#endregion
